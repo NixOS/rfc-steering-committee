1ac6921947557259136186701add830038b8704b
Linus Heckemann <git@sphalerite.org>
2021-09-23 16:21:01 +0200

    scripts: add support for paginated responses
    Thanks hub, for making it easy!

--- nominations.sh
+++ nominations.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p jq curl
+#!nix-shell -i bash -p jq gitAndTools.hub
 
-set -euo pipefail
+set -exuo pipefail
 pr="${1?usage: $0 <RFC number>}"
-curl -fsSL "https://api.github.com/repos/nixos/rfcs/issues/$pr/comments?per_page=100" | jq -rf nominations.jq
-curl -fsSL "https://api.github.com/repos/nixos/rfcs/pulls/$pr/comments?per_page=100" | jq -rf nominations.jq
+hub api --paginate "repos/nixos/rfcs/issues/$pr/comments?per_page=100" | jq -rf nominations.jq
+hub api --paginate "repos/nixos/rfcs/pulls/$pr/comments?per_page=100" | jq -rf nominations.jq
